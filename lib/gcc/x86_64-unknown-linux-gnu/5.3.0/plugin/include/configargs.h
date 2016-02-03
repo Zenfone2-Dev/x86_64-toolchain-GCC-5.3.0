b60d601e91500395346615be8d22c9b1c47d78b4
Sayeed <sayeed.afridi2009@gmail.com>
2016-02-03 20:34:16 +0530

    x86_64 GCC 5.3.0 TOOLCHAIN
    compiled from sources

---
+++ lib/gcc/x86_64-unknown-linux-gnu/5.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-5.3.0/configure --target= --prefix=/home/sayeed/toolchain/sayeed/ --with-newlib --with-gnu-as --with-gnu-ld --enable-languages=c,c++ --disable-multilib --enable-shared --disable-libssp";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
